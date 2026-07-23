1d6b7aa09837266c0ca1d57bfc5a037ce0e991b2
jtolli2 <jeffreytolliver@yahoo.com>
2026-07-22 23:54:26 -0400

    fix: configure api proxy upstream

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -2,6 +2,8 @@
 set -eu
 
 : "${POCKETBASE_HOST:=pocketbase:8090}"
+: "${POCKETBASE_UPSTREAM:=http://${POCKETBASE_HOST}}"
 export POCKETBASE_HOST
-envsubst '${POCKETBASE_HOST}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
+export POCKETBASE_UPSTREAM
+envsubst '${POCKETBASE_UPSTREAM}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
